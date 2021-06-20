0ba4fd889cae3c347383ffb9720e04b5890501f1
Mahdi <mahdi57.kamali1@gmail.com>
2021-06-20 23:53:49 +0430

    Add model City and Road

---
+++ final.py
@@ -0,0 +1,32 @@
+class City:
+    def __init__(self, id: int, name: str):
+        self.__id = id
+        self.__name = name
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def name(self):
+        return self.__name
+    
+
+class Road:
+    def __init__(self, id: int, name: str, start: int, to: int, through: list, speed_limit: int, length: int, bi_directional: bool):
+        self.__id = id
+        self.__name = name
+        self.start = start
+        self.to = to
+        self.through = through
+        self.speed_limit = speed_limit
+        self.length = length
+        self.bi_directional = bi_directional
+    
+    @property
+    def id(self):
+        return self.__id
+    
+    @property
+    def name(self):
+        return self.__name
