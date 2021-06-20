654456f6a62d127d6498d4b4e84842621f1579dd
Mahdi <mahdi57.kamali1@gmail.com>
2021-06-21 01:01:51 +0430

    Add class UserInterface
    Change __init__ method of City and Road models. Add methods for getting input and show output in UserInterface class.

--- final.py
+++ final.py
@@ -1,7 +1,7 @@
 class City:
-    def __init__(self, id: int, name: str):
-        self.__id = id
-        self.__name = name
+    def __init__(self, *args, **kwargs):
+        self.__id = kwargs.get('id')
+        self.__name = kwargs.get('name')
     
     @property
     def id(self):
@@ -10,18 +10,25 @@ class City:
     @property
     def name(self):
         return self.__name
-    
+
+    @classmethod
+    def field_names(cls) -> list:
+        city = City({})
+        fields = city.__dict__.keys()
+        del(city)
+        return fields
+
 
 class Road:
-    def __init__(self, id: int, name: str, start: int, to: int, through: list, speed_limit: int, length: int, bi_directional: bool):
-        self.__id = id
-        self.__name = name
-        self.start = start
-        self.to = to
-        self.through = through
-        self.speed_limit = speed_limit
-        self.length = length
-        self.bi_directional = bi_directional
+    def __init__(self, *args, **kwargs):
+        self.__id = kwargs.get('id')
+        self.__name = kwargs.get('name')
+        self.__from = kwargs.get('from')
+        self.__to = kwargs.get('to')
+        self.__through = kwargs.get('through')
+        self.__speed_limit = kwargs.get('speed_limit')
+        self.__length = kwargs.get('length')
+        self.__bi_directional = kwargs.get('bi_directional')
     
     @property
     def id(self):
@@ -30,6 +37,16 @@ class Road:
     @property
     def name(self):
         return self.__name
+
+    @classmethod
+    def field_names(cls) -> list:
+        road = Road({})
+        fields = road.__dict__.keys()
+        del(road)
+        return fields
+
+    def is_bi_directional(self):
+        return self.__bi_directional
 
 class Agency:
     def __init__(self):
@@ -41,3 +58,60 @@ class Agency:
     
     def add_city(self, city: City):
         self.__cities.append(city)
+
+class UserInterface:
+    def __init__(self):
+        self.agency = Agency()
+    
+    def get_input(self):
+        return input()
+    
+    def show_main_menu(self):
+        print("Main Menu - Select an action:")
+        print("1. Help")
+        print("2. Add")
+        print("3. Delete")
+        print("4. Path")
+        print("5. Exit")
+
+    def show_help(self):
+        print("Select a number from shown menu and enter. For example 1 is for help.")
+    
+    def show_add_delete_menu(self):
+        print("Select model:")
+        print("1. City")
+        print("2. Road")
+
+    def handle_help_cmd(self):
+        self.show_help()
+    
+    def handle_add_cmd(self):
+        self.show_add_delete_menu()
+        select = int(self.get_input())
+        if select == 1:
+            self.handle_add_city()
+        elif select == 2:
+            self.handle_add_road()
+
+    def handle_delete_cmd(self):
+        self.show_add_delete_menu()
+        select = int(self.get_input())
+        if select == 1:
+            self.handle_delete_city()
+        elif select == 2:
+            self.handle_delete_road()
+
+    def handle_path_cmd(self):
+        pass
+    
+    def exit(self):
+        pass
+
+    def handle_add_city(self):
+        kwargs = {}
+        for field in City.field_names():
+            if '_City__' in field:
+                field = field[7:]
+            print(f"{field}=?", end='')
+            kwargs[field] = self.get_input()
+        self.agency.add_city(City(kwargs))
