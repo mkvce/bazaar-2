c121665f2249f7319746d4a44eecc3cdfe3ba3ab
Mahdi <mahdi57.kamali1@gmail.com>
2021-06-21 13:23:32 +0430

    Handle path command

--- final.py
+++ final.py
@@ -59,6 +59,36 @@ class Road:
     def __eq__(self, value):
         return self.id == value.id
 
+    def __calculate_time(self, src_index: int, dst_index: int) -> dict:
+        hours = abs(dst_index - src_index) / len(self.__through) * (self.__length / self.__speed_limit)
+        minutes = hours * 60
+        m = minutes % 60
+        minutes -= m
+        h = minutes % (24 * 60)
+        minutes -= 60 * h
+        d = minutes // (24 * 60)
+        return {'days': d, 'hours': h, 'minutes': m}
+
+    def get_path(self, src: int, dst: int) -> dict:
+        way = self.__through
+        way.append(self.__to)
+        dst_index = -1
+        index = 0
+        while index < len(way) and self.__through[index] != src:
+            index += 1
+        src_index = index
+        index = len(way) - 1
+        while index >= 0 and self.__through[index] != dst:
+            index -= 1
+        dst_index = index
+        result = {}
+        if not self.bi_directional and src_index > dst_index:
+            return {'status': False}
+        if src_index == len(way) or dst_index == -1:
+            return {'status': False}
+        return {'status': True, 'road_name': self.name, 'time': self.__calculate_time(src_index, dst_index)}   
+
+
 class Agency:
     def __init__(self):
         self.__roads = []
@@ -96,9 +126,24 @@ class Agency:
         else:
             raise ModelNotFoundError('Road', id)
 
+    def get_city_name(self, id: int):
+        for city in self.__cities:
+            if city.id == id:
+                return city.name
+        raise ModelNotFoundError('City', id)
+
+    def get_pathes(self, src: int, dst: int) -> list:
+        pathes = []
+        for road in self.__roads:
+            path = road.get_path()
+            if path.get('status'):
+                pathes.append(path)
+        return pathes
+
+
 class UserInterface:
-    def __init__(self):
-        self.agency = Agency()
+    def __init__(self, agency: Agency):
+        self.agency = agency
     
     def get_input(self):
         return input()
@@ -132,23 +177,17 @@ class UserInterface:
         self.show_add_delete_menu()
         select = int(self.get_input())
         if select == 1:
-            self.handle_add_city()
+            self.handle_add_model('City')
         elif select == 2:
-            self.handle_add_road()
+            self.handle_add_model('Road')
 
     def handle_delete_cmd(self):
         self.show_add_delete_menu()
         select = int(self.get_input())
         if select == 1:
-            self.handle_delete_city()
+            self.handle_delete_model('City')
         elif select == 2:
-            self.handle_delete_road()
-
-    def handle_path_cmd(self):
-        pass
-    
-    def exit(self):
-        pass
+            self.handle_delete_model('Road')
 
     def __get_model_fields(self, model: str) -> list:
         if model == 'City':
@@ -186,4 +225,17 @@ class UserInterface:
             print(f"{err.model_name} with id {err.id} not found!")
         else:
             print(f"{model}:{id} deleted!")
+        self.show_main_menu()
+
+    def handle_path_cmd(self):
+        src_id, dst_id = [int(x) for x in self.get_input().split(':')]
+        src_city_name = self.agency.get_city_name(src_id)
+        dst_city_name = self.agency.get_city_name(dst_id)
+        pathes = self.agency.get_pathes(src_id, dst_id)
+        for path in pathes:
+            road_name = path['road_name']
+            dd = path['time']['days']
+            hh = path['time']['hours']
+            mm = path['time']['minutes']
+            print(f"{src_city_name}:{dst_city_name} via Road {road_name}: Takes {dd:.2d}:{mm:.2d}:{mm:.2d}")
         self.show_main_menu()
